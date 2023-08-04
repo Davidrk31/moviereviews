8681b0872c6a5cd38faf8e8aa29cfce8ee5884a4
David Alvarez <davidag3108@gmail.com>
2023-08-03 22:14:29 -0500

    about y color

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    #return HttpResponse('<h1>Welcome to home Page</h1>')
-    return render(request,"home.html",{"name":"Son Goku"})
+    return render(request,"home.html",{"name":"David Alvarez"})
     
+def about(request):
+    return render(request,"about.html")
