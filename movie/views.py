b40aaa69129edb8ac44351c4ea99d65b0e7e48d3
David Alvarez <davidag3108@gmail.com>
2023-08-27 01:42:57 -0500

    taller 2

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Movie
+
 def home(request):
     #return HttpResponse('<h1>Welcome to home Page</h1>')
-    return render(request,"home.html",{"name":"David Alvarez"})
+    #return render(request,"home.html",{"name":"David Alvarez"})
+    searchTerm = request.GET.get("searchMovie")
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies= Movie.objects.all()
+    return render (request,"home.html", {"searchTerm":searchTerm, "movies":movies })
+
 def about(request):
     return render(request,"about.html")
